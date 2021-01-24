94fc769f3b268ff1ea0de85495ec76b84fb35c3d
devalladares <60202683+devalladares@users.noreply.github.com>
2021-01-24 01:38:09 -0500

    Update main.js

--- main.js
+++ main.js
@@ -214,24 +214,24 @@ function setLoop() {
 		render();
 	});
 
-	const loader3 = new  FontLoader();
+	// const loader3 = new  FontLoader();
 
-	loader3.load( 'textures/helvetiker_regular.typeface.json', function ( font ) {
-
-		const geometry = new  TextGeometry( 'Hello three.js!', {
-			font: font,
-			size: 80,
-			height: 5,
-			curveSegments: 12,
-			bevelEnabled: true,
-			bevelThickness: 10,
-			bevelSize: 8,
-			bevelOffset: 0,
-			bevelSegments: 5
-		} );
- 
-
-	} );
+	// loader3.load( 'textures/helvetiker_regular.typeface.json', function ( font ) {
+	//
+	// 	const geometry = new  TextGeometry( 'Hello three.js!', {
+	// 		font: font,
+	// 		size: 80,
+	// 		height: 5,
+	// 		curveSegments: 12,
+	// 		bevelEnabled: true,
+	// 		bevelThickness: 10,
+	// 		bevelSize: 8,
+	// 		bevelOffset: 0,
+	// 		bevelSegments: 5
+	// 	} );
+	//
+	//
+	// } );
 
 	//////////////////////////////
 	//
